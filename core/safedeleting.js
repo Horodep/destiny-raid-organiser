7f157627a95c6d5b4ef1ac56e31ec0595209b66f
Horodep <github@horodep.me>
2022-12-03 22:58:04 +0300

    SafeDeleteMessage simpifying

--- core/safedeleting.js
+++ core/safedeleting.js
@@ -2,9 +2,6 @@ export function SafeDeleteMessageByTimeout(message, timeout){
     setTimeout(() => SafeDeleteMessage(message), timeout);
 }
 
-export async function SafeDeleteMessage(message){
-    var fetchedMessage = await message.channel.messages
-        .fetch(message.id)
-        .catch(() => null);
-    fetchedMessage?.delete(); 
+export function SafeDeleteMessage(message){
+    message?.delete().catch(() => null);
 }
